e723fb2c2d1af31b30e6d92d364e6f4645cae31e
Eduardo-Brito1 <kabisashi2@gmail.com>
2024-02-14 10:42:37 -0300

    exercicio 5 feito

---
+++ 05/index.js
@@ -0,0 +1,37 @@
+const horario = {
+    aulas: [
+        {
+            identificador: 1,
+            nome_da_aula: "Introdução a programação"
+        },
+        {
+            identificador: 2,
+            nome_da_aula: "Variáveis"
+        },
+        {
+            identificador: 3,
+            nome_da_aula: "Condicionais"
+        },
+        {
+            identificador: 4,
+            nome_da_aula: "Arrays"
+        }
+    ]
+
+
+}
+
+const curso = {
+    id: 1234,
+    nome: "Lógica de programação",
+    aulas: [],
+}
+
+for (let aula of horario.aulas) {
+    curso.aulas.push(aula)
+
+}
+
+
+
+console.log(curso)
