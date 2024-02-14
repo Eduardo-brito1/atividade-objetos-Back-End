868dc286e8cf443726b9104153df462facc26f16
Eduardo-Brito1 <kabisashi2@gmail.com>
2024-02-14 10:09:50 -0300

    subindo as atividades antes de começar

---
+++ 01/index.js
@@ -0,0 +1,18 @@
+const endereco = {
+    rua: "rua antonio custodio",
+    numero: 67,
+    complemento: "bloco 8 apartamento 203",
+    bairro: "serra verde",
+    cep: 21046649,
+    cidade: "Belo Horizonte",
+    estado: "Minas Gerais",
+    pais: "Brasil"
+}
+
+const usuario = {
+    nome: "Eduardo Brito",
+    email: "eduardopbrito@hotmail.com",
+    telefone: "3457104011",
+    data_nascimento: "21/01/2002",
+    endereco
+}
