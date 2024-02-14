6328dd6a7f954ada479df9cc365a781e6879c490
Eduardo-Brito1 <kabisashi2@gmail.com>
2024-02-14 10:16:11 -0300

    exercicio 2 feito

---
+++ 02/index.js
@@ -0,0 +1,25 @@
+const carro1 = {
+    marca: 'Peugeot',
+    modelo: '207',
+    ano: 2009,
+    cor: "preto",
+    quantidadeDePortas: 4,
+    automatico: false
+}
+
+const carro2 = {
+    marca: 'Renault ',
+    modelo: 'Logan',
+    ano: 2017,
+    cor: "preto",
+    quantidadeDePortas: 4,
+    automatico: true
+}
+const carro3 = {
+    marca: 'Volkswagen ',
+    modelo: 'Gol',
+    ano: 2022,
+    cor: "vermelho",
+    quantidadeDePortas: 4,
+    automatico: false
+}
