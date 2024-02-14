83d0a44ced171e96c6897a2d699ad6e095bb2cab
Eduardo-Brito1 <kabisashi2@gmail.com>
2024-02-14 11:17:38 -0300

    exercicio 8 pronto

---
+++ 07/index.js
@@ -0,0 +1,51 @@
+const livro = {
+    nome: 'O poder do hábito',
+    capitulos: [
+        {
+            nome: "O loop do hábito",
+            numero: 0
+        },
+        {
+            nome: "O cérebro ansioso",
+            numero: 0
+
+        },
+        {
+            nome: "A regra de ouro da mudança de hábito",
+            numero: 0
+
+        },
+        {
+            nome: "Hábitos angulares, ou a balada de Paul O`neill",
+            numero: 0
+        },
+        {
+            nome: "Starbucks e o hábito do sucesso",
+            numero: 0
+        },
+        {
+            nome: "O poder de uma crise",
+            numero: 0
+        },
+        {
+            nome: "Como a target sabe o que você quer antes que você saiba",
+            numero: 0
+        },
+        {
+            nome: "A Saddleback Church e o boicote aos ônibus de Montgomery",
+            numero: 0
+        },
+        {
+            nome: "A neorologia do livre-arbítrio",
+            numero: 0
+        },
+    ]
+}
+
+let contador = 0
+
+for (let i = 0; i < livro.capitulos.length; i++) {
+    contador = contador + 1
+    livro.capitulos[i].numero = contador
+}
+console.log(livro.capitulos)
